9a5c151f44a24b0df94c37a7e580908d00fa5c1d
Waleed Khan <wk5275173@gmail.com>
2026-09-03 05:22:12 +0100

    "fith commit"

--- my-app/src/components/CountryCard.jsx
+++ my-app/src/components/CountryCard.jsx
@@ -39,7 +39,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export default function CountryCard({name, flag, population, region, capital, data}) {  
   return (
-    <Link className="country-card" to={`/${name}`} state={data}> 
+    <Link className="country-card" to={`/${name}`} state={data}>          //Where to navigate to= and //What data to pass state
         <img src={flag} alt={`${name} flag`} />
           <div className="card-text">
               <h3 className="card-title">{name}</h3>
